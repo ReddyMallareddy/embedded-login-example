9414f3c1654fbcff1a124376378f8421818b24bc
ReddyMallareddy <2016956+ReddyMallareddy@users.noreply.github.com>
2019-08-04 08:03:08 +0530

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -5,6 +5,10 @@
 	<meta name="salesforce-mode" content="<?php echo getenv('SALESFORCE_MODE');?>-callback">
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>">
+	<meta name="salesforce-server-starturl" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/services/oauth2/authorize?response_type=token&&
+client_id=<?php echo getenv('SALESFORCE_CLIENT_ID');?>&
+redirect_uri=https://<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>/_callback.html&state=/">
+	<meta name="salesforce-self-register-starturl-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_STARTURL_ENABLED');?>">
 	<script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
 </head> 
 <body></body>    
