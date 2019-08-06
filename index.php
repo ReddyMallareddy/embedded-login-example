c919a4a3f0d2d7f5a5523e65d6cfa40c6c1c755f
ReddyMallareddy <2016956+ReddyMallareddy@users.noreply.github.com>
2019-08-06 20:44:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
     <meta name="salesforce-save-access-token" content="true">
     <meta name="salesforce-forgot-password-enabled" content="<?php echo getenv('SALESFORCE_FORGOT_PASSWORD_ENABLED');?>">
     <meta name="salesforce-self-register-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_ENABLED');?>">
- <!--   <meta name="salesforce-self-register-starturl-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_STARTURL_ENABLED');?>">
+    <meta name="salesforce-self-register-starturl-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_STARTURL_ENABLED');?>">
     <meta name="salesforce-server-starturl" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/services/oauth2/authorize?response_type=token&&
 client_id=<?php echo getenv('SALESFORCE_CLIENT_ID');?>&
-redirect_uri=https://<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>/_callback.html&state=/">  -->
-    <meta name="salesforce-self-register-starturl-enabled" content="true">
+redirect_uri=https://<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>/_callback.html&state=/">  
+<!--    <meta name="salesforce-self-register-starturl-enabled" content="true">-->
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
 	<link href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
